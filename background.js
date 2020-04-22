28d5116ea95e6fe419c750cc00e3b158a00169f4
Chengxuan Xing <cxing@uk.ibm.com>
2020-04-22 09:20:49 +0100

    check before set profile

--- background.js
+++ background.js
@@ -5,64 +5,68 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({
-    toggles: {
-      applycolour: true,
-      hideoptions: false
-    },
-    profile: {
-      "name": "zenhub",
-      "className": "zh-app__workspace",
-      "colour": {
-        "widgets": [{
-          "className": "zhc-issue-card",
-          "rules": [{
-            "colour": "lightgreen",
-            "textMatchers": [{
-              "className": "zhc-issue-card__repo-name",
-              "endsWith": ['-planning', '-stories']
-            }, {
-              "className": "zhc-label",
-              "equal": ['story', 'green', 'epic']
+  chrome.storage.sync.get('profile', function(data) {
+    if (!data.profile) {
+      chrome.storage.sync.set({
+        toggles: {
+          applycolour: true,
+          hideoptions: false
+        },
+        profile: {
+          "name": "zenhub",
+          "className": "zh-app__workspace",
+          "colour": {
+            "widgets": [{
+              "className": "zhc-issue-card",
+              "rules": [{
+                "colour": "lightgreen",
+                "textMatchers": [{
+                  "className": "zhc-issue-card__repo-name",
+                  "endsWith": ['-planning', '-stories']
+                }, {
+                  "className": "zhc-label",
+                  "equal": ['story', 'green', 'epic']
+                }]
+              }, {
+                "colour": "pink",
+                "textMatchers": [{
+                  "className": "zhc-issue-card__repo-name",
+                  "endsWith": ['-support', '-incidents', '-test', '-tickets']
+                }, {
+                  "className": "zhc-label",
+                  "equal": ['production issue', 'pipeline break', 'critical', 'build break', 'pink']
+                }]
+              }, {
+                "colour": "lightyellow",
+                "textMatchers": [{
+                  "className": "zhc-issue-card__repo-name",
+                  "endsWith": ['-debt', '-chore', '-usability']
+                }, {
+                  "className": "zhc-label",
+                  "equal": ['chore', 'enhancement', 'usability', 'bug', 'yellow']
+                }]
+              }, {
+                "colour": "lightblue",
+                "textMatchers": [{
+                  "className": "zhc-issue-card__repo-name",
+                  "endsWith": ['-rca-tasks', '-rca']
+                }, {
+                  "className": "zhc-label",
+                  "equal": ['rca-improvement', 'rca', 'blue']
+                }]
+              }]
             }]
-          }, {
-            "colour": "pink",
-            "textMatchers": [{
-              "className": "zhc-issue-card__repo-name",
-              "endsWith": ['-support', '-incidents', '-test', '-tickets']
+          },
+          "hide": {
+            "widgets": [{
+              "className": "zhc-sidebar"
             }, {
-              "className": "zhc-label",
-              "equal": ['production issue', 'pipeline break', 'critical', 'build break', 'pink']
+              "className": "zhc-board__menu"
             }]
-          }, {
-            "colour": "lightyellow",
-            "textMatchers": [{
-              "className": "zhc-issue-card__repo-name",
-              "endsWith": ['-debt', '-chore', '-usability']
-            }, {
-              "className": "zhc-label",
-              "equal": ['chore', 'enhancement', 'usability', 'bug', 'yellow']
-            }]
-          }, {
-            "colour": "lightblue",
-            "textMatchers": [{
-              "className": "zhc-issue-card__repo-name",
-              "endsWith": ['-rca-tasks', '-rca']
-            }, {
-              "className": "zhc-label",
-              "equal": ['rca-improvement', 'rca', 'blue']
-            }]
-          }]
-        }]
-      },
-      "hide": {
-        "widgets": [{
-          "className": "zhc-sidebar"
-        }, {
-          "className": "zhc-board__menu"
-        }]
-      }
-
+          }
+    
+        }
+      });
     }
-  });
+});
 });
